5153669869a3516cbf6a67dcbe961af03c88aa16
butch cosio <butchcosio@yahoo.com>
2018-01-08 20:27:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// write your code below!
+function happyHolidays() { 
+  return ("Happy holidays!")
+}
+  
+ function happyHolidaysTo(name) {
+   if (name==='you')
+   return `Happy holidays, {$name}!`
+   
+ }
+  
+  // write your code below!
