f1491445357845f99bd37077ea89a93dee663358
butch cosio <butchcosio@yahoo.com>
2018-01-08 20:54:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ function happyHolidays() {
    return `Happy ${holiday}, ${name}!`
   }
   
-  function happyHolidayTo(holiday, name) {
+  function holidayCountdown(holiday, days) {
     return `Happy ${holiday}, ${name}!`
   }
   
-  
+  describe('holidayCountdown(holiday, days)', () => {
+    it('returns "It\'s ${days} days until ${holiday}!"', () => {
+      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
   
   // write your code below!
